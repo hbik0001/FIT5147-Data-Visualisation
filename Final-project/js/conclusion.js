139d475cd6ef5c65abfb85a3c3ba72ee253cf8c2
Hari Krishna Bikki <hbik0001@student.monash.edu>
2019-06-22 15:38:13 +1000

    Update conclusion.js
    changed url variable

--- Final-project/js/conclusion.js
+++ Final-project/js/conclusion.js
@@ -202,7 +202,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
         function updateRadialTree(selComp) {
             var final_filtered_year = "https://media.githubusercontent.com/media/hbik0001/FIT5147-Data-Visualisation/master/data/final_filtered-year.csv";
-            d3.csv(final_filtered - year, function (d) {
+            d3.csv(final_filtered_year, function (d) {
 
                 var years = ["2013", "2014", "2015", "2016", "2017"]
 
